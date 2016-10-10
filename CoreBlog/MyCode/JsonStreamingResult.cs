d423f0bc2711bdbefae2947d024a095855907924
ststeiger <quandary82@hailmail.net>
2016-10-10 17:34:30 +0200

    Added dapper

---
+++ CoreBlog/MyCode/JsonStreamingResult.cs
@@ -0,0 +1,42 @@
+﻿
+namespace CoreBlog.MyCode
+{
+
+    // http://stackoverflow.com/questions/26269438/streaming-large-list-of-data-as-json-format-using-json-net
+    public class JsonStreamingResult : System.Web.Mvc.ActionResult
+    {
+        private System.Collections.IEnumerable itemsToSerialize;
+
+        public JsonStreamingResult(System.Collections.IEnumerable itemsToSerialize)
+        {
+            this.itemsToSerialize = itemsToSerialize;
+        }
+
+        public override void ExecuteResult(System.Web.Mvc.ControllerContext context)
+        {
+            System.Web.HttpResponseBase response = context.HttpContext.Response;
+            response.ContentType = "application/json";
+            response.ContentEncoding = System.Text.Encoding.UTF8;
+
+            Newtonsoft.Json.JsonSerializer serializer = new Newtonsoft.Json.JsonSerializer();
+
+            using (System.IO.StreamWriter sw = new System.IO.StreamWriter(response.OutputStream))
+            {
+                using (Newtonsoft.Json.JsonTextWriter writer = new Newtonsoft.Json.JsonTextWriter(sw))
+                {
+                    writer.WriteStartArray();
+                    foreach (object item in itemsToSerialize)
+                    {
+                        Newtonsoft.Json.Linq.JObject obj = 
+                            Newtonsoft.Json.Linq.JObject.FromObject(item, serializer);
+                        obj.WriteTo(writer);
+                        writer.Flush();
+                    } // Next item 
+                    writer.WriteEndArray();
+
+                } // End using writer 
+
+            } // End Using sw
+        }
+    }
+}
--- Dapper/Properties/AssemblyInfo.cs
+++ Dapper/Properties/AssemblyInfo.cs
@@ -2,34 +2,35 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-// Allgemeine Informationen zu einer Assembly werden über die folgenden
+// Allgemeine Informationen über eine Assembly werden über die folgenden 
 // Attribute gesteuert. Ändern Sie diese Attributwerte, um die Informationen zu ändern,
 // die mit einer Assembly verknüpft sind.
-[assembly: AssemblyTitle("CoreBlog")]
+[assembly: AssemblyTitle("Dapper")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("CoreBlog")]
+[assembly: AssemblyProduct("Dapper")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
-// Durch Festlegen von "ComVisible" auf "false" werden die Typen in dieser Assembly unsichtbar
-// für COM-Komponenten. Wenn Sie auf einen Typ in dieser Assembly aus
+// Durch Festlegen von ComVisible auf "false" werden die Typen in dieser Assembly unsichtbar 
+// für COM-Komponenten.  Wenn Sie auf einen Typ in dieser Assembly von 
 // COM zugreifen müssen, legen Sie das ComVisible-Attribut für diesen Typ auf "true" fest.
 [assembly: ComVisible(false)]
 
-// Die folgende GUID bestimmt die ID der "typelib", wenn dieses Projekt für COM verfügbar gemacht wird.
-[assembly: Guid("993c0b9d-f65e-4fee-939b-1ee23a0ac958")]
+// Die folgende GUID bestimmt die ID der Typbibliothek, wenn dieses Projekt für COM verfügbar gemacht wird
+[assembly: Guid("8fe1c317-ff19-4d74-ae06-e4131b74d871")]
 
 // Versionsinformationen für eine Assembly bestehen aus den folgenden vier Werten:
 //
 //      Hauptversion
-//Nebenversion
+//      Nebenversion 
 //      Buildnummer
 //      Revision
 //
-// Sie können alle Werte angeben oder die standardmäßigen Revisions- und Buildnummern
+// Sie können alle Werte angeben oder die standardmäßigen Build- und Revisionsnummern 
 // übernehmen, indem Sie "*" eingeben:
+// [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("2016.10.10.1603")]
 [assembly: AssemblyFileVersion("2016.10.10.1603")]
